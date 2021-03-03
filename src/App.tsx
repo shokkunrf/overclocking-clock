17f03b0f9db72aaffc6f394ea5b203420eaa72b2
shokkunrf <19404989+shokkunrf@users.noreply.github.com>
2021-03-03 22:45:57 +0900

    中央表示のスタイルを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
-import './App.css';
+import { css } from '@emotion/react';
+
 import { Clock } from 'container/clock';
 
+const mainStyle = css`
+  align-items: center;
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  text-align: center;
+`;
+
 const App = (): JSX.Element => (
-  <div className="App">
-    <header className="App-header">
-      <Clock />
-    </header>
+  <div className="App" css={mainStyle}>
+    <Clock />
   </div>
 );
 
